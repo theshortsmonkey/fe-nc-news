a4a1001b1a6e60196ef8c037b932ccdf9530b3bf
theshortsmonkey <j.horsley72@gmail.com>
2024-03-11 16:17:21 +0000

    updated ArticleCard with image and implemented a loading stats

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getArticleById } from '../utils/api'
 import { ArticleCard } from './ArticleCard/ArticleCard'
+import { Loading } from './Loading'
 
 export const SingleArticle = () => {
   const { article_id } = useParams()
   const [article, setArticle] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
+    setIsLoading(true)
     getArticleById(article_id).then((data) => {
       setArticle(data.article)
+      setIsLoading(false)
     })
-  })
+  },[])
   return (
     <article>
-      <ArticleCard size='large' article={article} />
+      {isLoading ? <Loading /> : <ArticleCard size='large' article={article} />}
     </article>
   )
 }
