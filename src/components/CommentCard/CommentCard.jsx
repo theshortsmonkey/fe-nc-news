0bf89517ac7e91cd6fa88ca23cb09b226ac91dda
theshortsmonkey <j.horsley72@gmail.com>
2024-03-12 09:43:50 +0000

    updated date output formatting

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { CurrUserContext } from "../../contexts/CurrUser"
 import './CommentCard.css'
+import { formatDate } from "../../utils/utils"
 
 export const CommentCard = ({ comment }) => {
   const { currUser } = useContext(CurrUserContext)
@@ -13,7 +14,7 @@ export const CommentCard = ({ comment }) => {
         <button>Up Vote</button>
         <button>Down Vote</button>
       </div>
-      <p id="comment-card-created">Created at: {comment.created_at}</p>
+      <p id="comment-card-created">Posted: {formatDate(comment.created_at)}</p>
       {currUser.username === comment.author ? (
         <div>
         <button id="comment-card-delete">Delete Comment</button>
