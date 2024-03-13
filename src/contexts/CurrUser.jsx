bc6f4e6d399131094d43166cac3201d9ac91aaa3
theshortsmonkey <j.horsley72@gmail.com>
2024-03-13 11:24:54 +0000

    changed default user to "none"

--- src/contexts/CurrUser.jsx
+++ src/contexts/CurrUser.jsx
@@ -3,9 +3,9 @@ import { createContext, useState } from "react";
 export const CurrUserContext = createContext()
 
 const defaultUser = {
-  username: 'cooljmessy',
-  name: 'Peter Messy',
-  avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
+  username: '',
+  name: '',
+  avatar_url: ''
 }
 
 export const CurrUserProvider = ({children}) => {
