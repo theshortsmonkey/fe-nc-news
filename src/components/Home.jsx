87e553e82678848192b4610ea3f11cf681ce7e82
theshortsmonkey <j.horsley72@gmail.com>
2024-03-14 15:48:33 +0000

    updated Home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ export const Home = () => {
     <section id='home'>
       <h1>Welcome to NC News by theShortsMonkey</h1>
       <h2>Have a browse round, enjoy catching up on the articles and posting your comments</h2>
+      <img src='https://images.unsplash.com/photo-1495020689067-958852a7765e?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='man sitting on a bench reading newspaper'/>
     </section>
   )
 }
